09ef48d335c7bc166229de7cd312e1e6fa3f6fbe
MacTheAnon <mac1379530@icloud.com>
2026-01-13 10:49:49 -0600

    Config: Update CORS to allow custom domain

--- backend-biz/src/main/java/com/mcintosh/mcintosh_digital_biz/config/WebConfig.java
+++ backend-biz/src/main/java/com/mcintosh/mcintosh_digital_biz/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://mcintosh-digital-solutions.up.railway.app")
+                .allowedOrigins("https://www.mcintoshdigital.com","https://mcintosh-digital-solutions.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
